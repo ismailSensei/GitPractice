7c84ee6b36a28e5d883f5c0bf8c634c3e2697d01
Ismo <olawaleismail8@gmail.com>
2023-03-25 16:32:41 +0100

    Changed Initialising File

--- cypress/e2e/spec-copy-1.cy.js
+++ cypress/e2e/spec-copy-1.cy.js
@@ -5,4 +5,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+  })
 })
